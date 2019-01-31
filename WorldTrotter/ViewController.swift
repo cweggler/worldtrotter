9519e2b8e37000c927dea038046a32be0e0c8754
Cara Weggler <cweggler@gmail.com>
2019-01-31 06:46:04 -0600

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Cara on 1/31/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
