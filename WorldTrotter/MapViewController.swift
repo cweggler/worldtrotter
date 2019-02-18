b0defdf70fcfd720a8bf1021b8ee6964308373f6
Cara Weggler <cweggler@gmail.com>
2019-02-18 17:56:58 -0600

    Ch. 6 changed code so map view is being created programmatically

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // create a map view
+        mapView = MKMapView()
+        
+        // set it as *the* view of this view controller
+        view = mapView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
