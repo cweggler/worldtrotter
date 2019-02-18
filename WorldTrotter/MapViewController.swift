8bd705fa3b741a08e6f39152905cd48bf3f95fc0
Cara Weggler <cweggler@gmail.com>
2019-02-18 10:57:23 -0600

    Created MapViewController file

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Cara on 2/18/19.
+//  Copyright © 2019 Cara. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
