c007bc78e41d8649232e11b8804f075a6743735f
Cara Weggler <cweggler@gmail.com>
2019-02-18 18:05:06 -0600

    Added UISegmentedControl

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -18,6 +18,16 @@ class MapViewController: UIViewController {
         
         // set it as *the* view of this view controller
         view = mapView
+        
+        // add a UISegmentedControl to allow the user to choose between different options for view
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        
+        // this is to turn off the older auto-resizing mask so it doesn't conflict
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(segmentedControl)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
